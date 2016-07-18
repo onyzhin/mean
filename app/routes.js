0ff67a7cc7edea2073527406b7b70e0320b4fac9
o.nyzhin <o.nyzhin@gmail.com>
2016-07-18 21:32:20 +0300

    fix

--- app/routes.js
+++ app/routes.js
@@ -123,6 +123,6 @@ module.exports = function (app) {
     });
 
     app.get('*', function (req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile(__dirname + '/public/index.html'); 
     });
 };
--- server.js
+++ server.js
@@ -1,31 +1,25 @@
-// set up ======================================================================
 var express = require('express');
-var app = express(); 						// create our app w/ express
-var mongoose = require('mongoose'); 				// mongoose for mongodb
-var port = process.env.PORT || 8080; 				// set the port
-var database = require('./config/database'); 			// load the database config
+var app = express(); 						
+var mongoose = require('mongoose'); 				
+var port = process.env.PORT || 8080; 				
+var database = require('./config/database'); 			
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// configuration ===============================================================
 mongoose.connect(database.localUrl, function function_name(err, success) {
-	console.log('___________');
 	console.log(err);
 	if (err) throw err;	
 })	
 
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+app.use(express.static(__dirname + '/public')); 		
+app.use(morgan('dev')); 
+app.use(bodyParser.urlencoded({'extended': 'true'})); /
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); 
+app.use(methodOverride('X-HTTP-Method-Override')); 
 
-
-// routes ======================================================================
 require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
